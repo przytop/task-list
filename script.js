400dde59d635f27885c3215cf02bb41f981a61f4
Pawel Przytomski <przytop@gmail.com>
2025-03-18 21:34:33 +0100

    Update script.js
    - Added function to add tasks through form submission
    - Added event listeners for adding, removing, and toggling tasks

--- script.js
+++ script.js
@@ -30,6 +30,44 @@
     render();
   };
 
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+
+    const input = document.querySelector(".form__input");
+    const value = input.value.trim();
+
+    if (value) {
+      addTask(value);
+      input.value = "";
+    }
+    input.focus();
+  };
+
+  const bindFormButtonListener = () => {
+    const form = document.querySelector(".form");
+
+    form.addEventListener("submit", onFormSubmit);
+  };
+
+  const bindTaskButtonsListeners = () => {
+    const toggleButtons = document.querySelectorAll(".task__button--toggle");
+    const removeButtons = document.querySelectorAll(".task__button--remove");
+
+    toggleButtons.forEach((button) => {
+      button.addEventListener("click", () => {
+        const taskId = Number(button.dataset.id);
+        toggleTask(taskId);
+      });
+    });
+
+    removeButtons.forEach((button) => {
+      button.addEventListener("click", () => {
+        const taskId = Number(button.dataset.id);
+        removeTask(taskId);
+      });
+    });
+  };
+
   const renderTasks = () => {
     const tasksList = document.querySelector(".tasks__list");
 
@@ -37,21 +75,32 @@
       .map(
         (task) => `
         <li class="task__element">
-          <button class="task__button task__button--toggle" data-id="${task.id}">${
-          task.done ? "✅" : "🔲"
-        }</button>
+          <button class="task__button task__button--toggle" data-id="${
+            task.id
+          }">${task.done ? "✅" : "🔲"}</button>
           <span class="task__text ${task.done ? "task__text--done" : ""}">${
           task.name
         }</span>
-          <button class="task__button task__button--remove" data-id="${task.id}">❌</button>
+          <button class="task__button task__button--remove" data-id="${
+            task.id
+          }">❌</button>
         </li>
         `
       )
       .join("");
+
+    bindTaskButtonsListeners();
+  };
+
+  const renderButtons = () => {};
+
+  const bindListeners = () => {
+    bindFormButtonListener();
   };
 
   const render = () => {
     renderTasks();
+    bindListeners();
   };
 
   const init = () => {
